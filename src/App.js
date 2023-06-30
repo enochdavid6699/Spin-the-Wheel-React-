155730a59cf1d6f27a5f217529eb6c9dd085cd00
Enoch <enochdavid6699@gmail.com>
2023-06-30 16:25:20 +0530

    Backbone working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [spinCount, setSpinCount] = useState(0);
+  const [isSpinning, setIsSpinning] = useState(false);
+  const [coupon, setCoupon] = useState('');
+
+  const spinWheel = () => {
+    // Only allow spinning when not already spinning
+    if (!isSpinning) {
+      setIsSpinning(true);
+      setSpinCount(spinCount + 1);
+
+      // Simulate wheel spinning for 2 seconds
+      setTimeout(() => {
+        setIsSpinning(false);
+        
+        // Determine the coupon prize based on spin count
+        let prize = '';
+        if (spinCount === 1) {
+          prize = 'Try Again';
+        } else if (spinCount === 2) {
+          prize = 'Gift Card';
+        }
+
+        setCoupon(prize);
+      }, 2000);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="coupon-wheel">
+        <div
+          className={`wheel ${isSpinning ? 'spinning' : ''}`}
+          onClick={spinWheel}
         >
-          Learn React
-        </a>
-      </header>
+          Spin the Wheel
+        </div>
+        <div className="coupon-prize">{coupon}</div>
+      </div>
     </div>
   );
 }
